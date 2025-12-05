7f000b8900a54d53ba14439d5a84b4d79aa540df
Andrea Puddu <andreapudd@gmail.com>
2025-12-05 16:35:42 +0100

    name main

--- src/aml_project/show_result.py
+++ src/aml_project/show_result.py
@@ -4,22 +4,26 @@ from aml_project.view import view_images
 from config import Config
 import torch
 from torch import nn
-criterion = nn.L1Loss()
-config = Config()
-device = config.detect_device()
-preprocessor = preprocess.Processor(config)
-with torch.no_grad():
-    model = model.Photosciop(config)
-    model.load_state_dict(torch.load("data/model_weights", map_location=torch.device("cpu")))
-    ds = dataset.ImageOnlyDataset(config, "validation")
-    img = preprocessor(ds[0].unsqueeze(0))
-    batch = img
-    inputs = mask_batch(batch, config, device)
-    inp = inputs[:, :3, :, :]
-    out = model(inputs)
-    images_to_plot = preprocessor.denorm(torch.cat((inp, out, img, img - out, inp - out) ))
-    # images_to_plot = (torch.cat((inputs[:, :3, :, :], model(inputs), img)))
-    
-    view_images(images_to_plot, ["input", "output", "expected", "out - expect", "inp - out"])
-    
 
+def main():
+    criterion = nn.L1Loss()
+    config = Config()
+    device = config.detect_device()
+    preprocessor = preprocess.Processor(config)
+    with torch.no_grad():
+        model = model.Photosciop(config)
+        model.load_state_dict(torch.load("data/model_weights", map_location=torch.device("cpu")))
+        ds = dataset.ImageOnlyDataset(config, "validation")
+        img = preprocessor(ds[0].unsqueeze(0))
+        batch = img
+        inputs = mask_batch(batch, config, device)
+        inp = inputs[:, :3, :, :]
+        out = model(inputs)
+        images_to_plot = preprocessor.denorm(torch.cat((inp, out, img, img - out, inp - out) ))
+        # images_to_plot = (torch.cat((inputs[:, :3, :, :], model(inputs), img)))
+        
+        view_images(images_to_plot, ["input", "output", "expected", "out - expect", "inp - out"])
+        
+
+if __name__ == "__main__":
+    main()
