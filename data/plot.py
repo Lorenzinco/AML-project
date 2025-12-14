281eecbddad643a976fcd9d47e4c43c0ed13b9e7
Andrea Puddu <andreapudd@gmail.com>
2025-12-14 17:33:12 +0100

    add d_loss to plot

--- data/plot.py
+++ data/plot.py
@@ -1,12 +1,14 @@
 from matplotlib import pyplot as plt
 from json import load
 
-js = load(open("loss_history.json"))
+js = load(open("hist.json"))
 
 tra = [x["train_loss"] for x in js]
 val = [x["val_loss"] for x in js]
-plt.plot(val, label="val")
+d = [x["d_loss"] for x in js]
 plt.plot(tra, label="train")
+plt.plot(val, label="val")
+plt.plot(d, label="discriminator")
 plt.legend()
 plt.show()
 
